b91d116fef3897249cc66ae8409e67ccd559d0a6
Eloy <117983001+eloy6lega@users.noreply.github.com>
2022-11-11 13:13:22 +0100

    Delete Tareas directory

--- Tareas/ciudad.py
+++
@@ -1,5 +0,0 @@
-ciudad=input('Dime una ciudad: ') #me preguntará una ciudad, y yo le digo una, según la que le diga me dirá lo que hemos programado
-if ciudad=='madrid': #si la ciudad es madird...
-    print('¡¡¡BIENVENIDO!!!') #dirá bienvenidos
-else: #si no es madrid
-    print('No te conozco...') #dirá que no nos conoce
