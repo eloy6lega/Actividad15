4f8725aa47eadd5536318348dc685d437b35ab8a
Eloy <117983001+eloy6lega@users.noreply.github.com>
2022-11-11 13:13:42 +0100

    Delete pseint_algoritmia directory

--- pseint_algoritmia/Suma.psc
+++
@@ -1,9 +0,0 @@
-Algoritmo Suma
-	Definir A,B,C Como Real;
-	Escribir 'dime el primer número';
-	Leer A;
-	Escribir 'dime el segundo número';
-	Leer B;
-	C <- A+B;
-	Escribir 'el resultado es ',C;
-FinAlgoritmo
--- pseint_algoritmia/asignaturasMedia.py
+++
@@ -1,8 +0,0 @@
-asignaturas=int(input('cuántas asignaturas tienes '))
-totalNota=0 #acumulador
-for i in range(asignaturas):
-    nota=int(input(f'dime la nota de la la asignatura {i} '))
-    #totalNota=totalNota+nota
-    totalNota+=nota
-notaMedia=totalNota/asignaturas
-print(f'tu nota media es {notaMedia}')
